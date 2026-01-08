be20d3c68972c3e8f22ca00b930b658954ab424e
GyeongHun <riverpower6@naver.com>
2026-01-08 17:04:45 +0900

    [CHORE] 깃허브 CI/CD 자동화
    - .github/workflows/deploy.yml 파일 작성을 통한 빌드 및 푸시 자동화
    - 현재는 중단 배포

--- src/main/kotlin/io/river/ec2_s3_deploy_prac/HomeController.kt
+++ src/main/kotlin/io/river/ec2_s3_deploy_prac/HomeController.kt
@@ -1,16 +1,19 @@
 package io.river.ec2_s3_deploy_prac
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class HomeController(
-    private val s3Service: S3Service
+    private val s3Service: S3Service,
+    @Value("\${custom.secret-word}")
+    private val secretWord: String
 ) {
 
     @GetMapping("/")
     fun main(): String {
-        return "Hi"
+        return "secretWord: $secretWord"
     }
 
     @GetMapping("/buckets")
